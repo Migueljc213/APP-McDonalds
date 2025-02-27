10a9d6609b8ef62286f01b732d1ed8dfaebd9b14
Jose Miguel <migueljcbrbm@gmail.com>
2025-02-27 14:38:01 -0300

    feat: component header menu

--- src/app/[slug]/menu/components/header.tsx
+++ src/app/[slug]/menu/components/header.tsx
@@ -1,19 +1,48 @@
-"use client"
+"use client";
 
+import { Button } from "@/components/ui/button";
 import { Restaurant } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
 import { useParams } from "next/navigation";
 import { useRouter } from "next/router";
 
-interface RestaurantHeaderProps{
-    restaraunt: Pick<Restaurant, "name" | "coverImageUrl">;
+interface RestaurantHeaderProps {
+  restaurant: Pick<Restaurant, "name" | "coverImageUrl">;
 }
 
-const RestarauntHeader = ({ restaraunt }: RestaurantHeaderProps) => {
-    const { slug } = useParams<{ slug: string }>;
-    const router = useRouter();
-    const handleBackClick = () => router.back();
-    const handleOrders
+const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
+  const { slug } = useParams<{ slug: string }>();
+  const router = useRouter();
+  const handleBackClick = () => router.back();
+  const handleOrdersClick = () => router.push(`/${slug}/orders`);
 
+  return (
+    <div className="relative h-[250px] w-full">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 z-50 rounded-full"
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
+      <Image
+        src={restaurant.coverImageUrl}
+        alt={restaurant.name}
+        fill
+        className="object-cover"
+      />
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 z-50 rounded-full"
+        onClick={handleOrdersClick}
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
+  );
 };
 
-export default RestarauntHeader;
+export default RestaurantHeader;
